4dec2081e0411883714b1c9e52ea426b9136bf26
Andrew Akers <andrew@akrs.me>
2020-06-02 20:32:20 -0700

    Add basic numeric support

--- playpen/__init__.py
+++ playpen/__init__.py
@@ -1,1 +1,27 @@
 __version__ = '0.1.0'
+
+import ast
+import operator as op
+from typing import Dict, Union, Optional, Any
+
+Number = Union[int, float]
+
+OPS = {
+    ast.Mult: op.mul, ast.Add: op.add
+}
+
+def eval_numeric(expression : str, /, variables : Optional[Dict[str, Number]] = None) -> Union[bool, Number]:
+    ops = {
+        ast.Mult: op.mul, ast.Add: op.add, ast.Sub: op.sub, ast.Pow: op.pow
+    }
+    return _eval(ast.parse(expression, mode='eval'), variables, ops)
+
+
+def _eval(tree, variables : Dict[str, Any], ops : Dict[Any, Any]) -> Any:
+    if type(tree) is ast.Constant:
+        return tree.value
+
+    b = tree.body
+    if type(b) is ast.BinOp:
+        return ops[type(b.op)](_eval(tree.body.left, variables, ops),
+                               _eval(tree.body.right, variables, ops))
--- tests/test_playpen.py
+++ tests/test_playpen.py
@@ -1,5 +1,12 @@
-from playpen import __version__
+from playpen import __version__, eval_numeric
 
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_simple_numeric_math():
+    assert eval_numeric('3 * 4') == 12
+    assert eval_numeric('3 + 4') == 7
+    assert eval_numeric('3 ** 4') == 81
+    assert eval_numeric('4 - 3') == 1
+
