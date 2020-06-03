aec8cf0692056249781128533fa061d768b58036
Andrew Akers <andrew@akrs.me>
2020-06-02 21:46:11 -0700

    Added support for compare

--- playpen/__init__.py
+++ playpen/__init__.py
@@ -7,21 +7,44 @@ from typing import Dict, Union, Optional, Any
 Number = Union[int, float]
 
 OPS = {
-    ast.Mult: op.mul, ast.Add: op.add
+    ast.Mult: op.mul, ast.Add: op.add,
 }
 
 def eval_numeric(expression : str, /, variables : Optional[Dict[str, Number]] = None) -> Union[bool, Number]:
+    tree = ast.parse(expression, mode='eval')
     ops = {
-        ast.Mult: op.mul, ast.Add: op.add, ast.Sub: op.sub, ast.Pow: op.pow
+        ast.Mult: op.mul, ast.Add: op.add, ast.Sub: op.sub, ast.Pow: op.pow,
+        ast.USub: op.neg,
+        ast.Lt: op.lt, ast.Gt: op.gt
     }
-    return _eval(ast.parse(expression, mode='eval'), variables, ops)
+    _eval = _eval_builder(variables, ops)
+    return _eval(tree)
 
 
-def _eval(tree, variables : Dict[str, Any], ops : Dict[Any, Any]) -> Any:
-    if type(tree) is ast.Constant:
-        return tree.value
+def _eval_builder(variables : Dict[str, Any], ops : Dict[Any, Any]):
+    def _compare(left, ops_list, comparators):
+        if not ops[type(ops_list[0])](_eval(left), _eval(comparators[0])):
+            return False
 
-    b = tree.body
-    if type(b) is ast.BinOp:
-        return ops[type(b.op)](_eval(tree.body.left, variables, ops),
-                               _eval(tree.body.right, variables, ops))
+        if len(ops_list) == 1:
+            return True
+
+        return _compare(comparators[0], ops[1:], comparators[1:])
+
+    def _eval(node):
+        if type(node) is ast.Constant:
+            return node.value
+        if type(node) is ast.Name:
+            return variables[node.id]
+
+        if type(node) is ast.Expression:
+            node = node.body
+
+        if type(node) is ast.BinOp:
+            return ops[type(node.op)](_eval(node.left), _eval(node.right))
+        if type(node) is ast.UnaryOp:
+            return ops[type(node.op)](_eval(node.operand))
+        if type(node) is ast.Compare:
+            return _compare(node.left, node.ops, node.comparators)
+
+    return _eval
--- tests/test_playpen.py
+++ tests/test_playpen.py
@@ -1,12 +1,25 @@
-from playpen import __version__, eval_numeric
+from playpen import eval_numeric
 
-
-def test_version():
-    assert __version__ == '0.1.0'
 
 def test_simple_numeric_math():
     assert eval_numeric('3 * 4') == 12
     assert eval_numeric('3 + 4') == 7
     assert eval_numeric('3 ** 4') == 81
     assert eval_numeric('4 - 3') == 1
+    assert eval_numeric('4 + -3') == 1
 
+
+def test_numeric_variables():
+    variables = {
+        'x': 3,
+        'y': 4
+    }
+
+    assert eval_numeric('x + y', variables=variables) == 7
+
+
+def test_parens():
+    assert eval_numeric('(1 + 2) * 3') == 9
+
+def test_compare():
+    assert eval_numeric('2 < 3') == True
